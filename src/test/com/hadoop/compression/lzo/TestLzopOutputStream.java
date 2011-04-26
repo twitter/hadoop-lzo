twitter/hadoop-lzo

commit 72b439b05407e05f9ae53aad4273244cb8eaaf7b

Issue 20: write uncompressed bytes if uncompressed size==compressed size
The LZO specification says that we should write the uncompressed bytes
rather than the compressed bytes if the compressed buffer is actually
larger than the uncompresesd buffer.

To conform to the standard, this means we have to write the uncompressed
bytes also when they have exactly the same size as the compressed bytes.

src/test/com/hadoop/compression/lzo/TestLzopOutputStream.java
@@ -46,6 +46,7 @@ public class TestLzopOutputStream extends TestCase {
   private final String bigFile = "100000.txt";
   private final String mediumFile = "1000.txt";
   private final String smallFile = "100.txt";
+  private final String issue20File = "issue20-lzop.txt";
 
   @Override
   protected void setUp() throws Exception {
@@ -82,6 +83,15 @@ public class TestLzopOutputStream extends TestCase {
   public void testSmallFile() throws NoSuchAlgorithmException, IOException,
   InterruptedException {    
     runTest(smallFile);
+  }
+
+  /**
+   * TODO: Write documentation.
+   *
+   */
+  public void testIssue20File() throws NoSuchAlgorithmException, IOException,
+  InterruptedException {
+    runTest(issue20File);
   }
 
   /**

src/java/com/hadoop/compression/lzo/LzopOutputStream.java
@@ -177,7 +177,7 @@ public class LzopOutputStream extends CompressorStream {
       // the LZO specification says that we should write the uncompressed bytes rather
       // than the compressed bytes.  The decompressor understands this because both sizes
       // get written to the stream.
-      if (compressor.getBytesRead() < compressor.getBytesWritten()) {
+      if (compressor.getBytesRead() <= compressor.getBytesWritten()) {
         // Compression actually increased the size of the buffer, so write the uncompressed bytes.
         byte[] uncompressed = ((LzoCompressor)compressor).uncompressedBytes();
         rawWriteInt(uncompressed.length);

commit 78e8199517ff187f8ec1628a700f70554c7280b5

Issue 20: add documentation for test case (introduced by commit 72b439b)

src/test/com/hadoop/compression/lzo/TestLzopOutputStream.java
@@ -86,7 +86,17 @@ public class TestLzopOutputStream extends TestCase {
   }
 
   /**
-   * TODO: Write documentation.
+   * The LZO specification says that we should write the uncompressed bytes
+   * rather than the compressed bytes if the compressed buffer is actually
+   * larger ('&gt;') than the uncompressed buffer.
+   *
+   * To conform to the standard, this means we have to write the uncompressed
+   * bytes also when they have exactly the same size as the compressed bytes.
+   * (the '==' in '&lt;=').
+   *
+   * The input data of this test is known to compress to the same size as the
+   * uncompressed data.  Hence we verify that we handle the boundary condition
+   * correctly.
    *
    */
   public void testIssue20File() throws NoSuchAlgorithmException, IOException,